06728ba46886a38ffc981f189a7c5c2869597fcd
nikhiljohnbejoy <nikhiljohnbejoy@yahoo.co.in>
2020-05-24 22:28:17 +0530

    lala

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 const http =require('http').Server(app)
 
 const io=require('socket.io')(http)
-const port=3000
+const port=3005
 
 app.use(express.static(__dirname+"/public"))
 let clients = 0;
