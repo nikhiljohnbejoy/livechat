89e983b172069a23c4562f2259a008a2754222ae
nikhiljohnbejoy <nikhiljohnbejoy@yahoo.co.in>
2020-05-24 17:18:12 +0530

    video stream

---
+++ public/main.js
@@ -0,0 +1,72 @@
+let Peer = require('simple-peer');
+let socket = io();
+const video = document.querySelector('video');
+let client = {};
+
+//get stream
+navigator.mediaDevices.getUserMedia({video : true, audio : true})
+.then(stream =>{
+    socket.emit('NewClient');
+    video.srcObject = stream;
+    video.play();
+    
+    //Used to Initialize a Peer
+    function InitPeer(type){
+        console.log('Init Peer '+ type)
+        let peer=new Peer({initiator : (type == 'init')?true:false, stream: stream, trickle: false})
+        peer.on('stream',function(stream){
+            CreateVideo(stream)
+        })
+        peer.on('close',function(){
+            document.getElementById("peerVideo").remove();
+            peer.destroy();
+        })
+        return peer;
+    }
+    //For Peer of type Init
+    function MakePeer(){
+        console.log('made peer')
+        client.gotAnswer=false;
+        let peer = InitPeer('init');
+        peer.on('signal',function(data){
+            if(!client.gotAnswer){
+                socket.emit('Offer',data)
+            }
+        })
+        client.peer=peer;
+    }
+    //For peer of type notInit
+    function FrontAnswer(offer){
+        console.log('FrontAnswer');
+        let peer=InitPeer('notInit');
+        peer.on('signal',(data)=>{
+            socket.emit('Answer',data);
+        })
+        peer.signal(offer)
+    }
+    function SignalAnswer(answer){
+        console.log('SignalAnswer');
+        client.gotAnswer=true;
+        let peer=client.peer;
+        peer.signal(answer)
+    }
+    function CreateVideo(stream){
+        console.log('createVideo');
+        let video=document.createElement('video')
+        video.id='peerVideo'
+        video.srcObject=stream;
+        video.class='embed-responsive-item'
+        document.querySelector('#peerDiv').appendChild(video);
+        video.play();
+    }
+    function sessionActive(){
+        console.log('session active')
+        document.write('Session Active. pls come back later');
+    }
+    socket.on('BackOffer', FrontAnswer)
+    socket.on('BackAnswer', SignalAnswer)
+    socket.on('SessionActive',sessionActive)
+    socket.on('CreatePeer',MakePeer)
+
+})
+.catch(err => document.write(err));
---
+++ server.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+const http =require('http').Server(app)
+
+const io=require('socket.io')(http)
+const port=process.env.PORT || 3000
+
+app.use(express.static(__dirname+"/public"))
+let clients = 0;
+
+io.on('connection', function(socket){
+    
+    socket.on("NewClient", function(){
+        
+        if(clients<2){
+            if(clients == 1){
+                console.log('emit peer')
+                this.emit('CreatePeer')
+            }
+        }
+        else{
+            this.emit('SessionActive')
+        }
+        clients++;
+        console.log('con'+clients)
+    })
+    socket.on('Offer',SendOffer)
+
+    socket.on('Answer',SendAnswer)
+
+    socket.on('disconnect',Disconnect)
+})
+
+
+    function Disconnect(){
+        if(clients > 0)
+        clients--;
+        
+        console.log('dis'+clients)
+    }
+    function SendOffer(offer){
+        console.log('SendOffer')
+        this.broadcast.emit('BackOffer',offer)
+    }
+    function SendAnswer(answer){
+        console.log('SendAnswer')
+        this.broadcast.emit('BackAnswer',answer)
+    }
+
+
+http.listen(port, () => console.log(`Active on ${port} port`))
+/*var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(8080);*/
